46248b648eff7f6a9158e8dd8d754db0bfd9a2ef
noguchi-taiki <taiki.13n1015@gmail.com>
2024-07-01 01:58:40 +0900

    mysqlへの接続及びスクレイピング先HPへのアクセスは可能
    改善事項:データの取得方法（21~24）が間違えているため空のデータを取ってきてしまう。

--- cafeScraping.js
+++ cafeScraping.js
@@ -1,8 +1,8 @@
 const puppeteer = require('puppeteer');
 const mysql = require('mysql2/promise');
 
-
 (async()=> {
+    try{
     const connection = await mysql.createConnection({
         host: "127.0.0.1",
         // prot: "3306",ポート番号は以前は必要だったらしいけど今はいらない（書くとエラーになる）
@@ -10,38 +10,42 @@ const mysql = require('mysql2/promise');
         user: "root",
         password: "root",
     })
+    console.log("mysqlに接続");
+    
 
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.goto('https://www.clubjt.jp/map');
-    console.log("https://www.clubjt.jp/map");
+    console.log("ページにアクセス");
 
     const cafeUrls = await page.evaluate(()=>{
         const links = Array.from(document.querySelectorAll('a[href*="cafe"]'));
-        console.log(links);
-        console.log("りんくん");
         return links.map(links => links.href);
     });
 
+    console.log(cafeUrls);
+
     for(const url of cafeUrls){
         const urlObj = new URL(url);
-        console.log(urlObj);
-        console.log("urlObjの表示");
         const latitude = urlObj.searchParams.get('lat');
-        console.log(latitude);
-        console.log("latの表示");
         const longitude = urlObj.searchParams.get('lng');
-        console.log(longitude);
-        console.log("longの表示");
+
+        console.log("URL:",url);
         
         if(latitude && longitude){
-            await connection.query(
-                'INSERT INTO cafes (url, latitude, longitude) VALUES (?, ?, ?)',
-                [url, parseFloat(latitude), parseFloat(longitude)]
-            );
+            try{
+                const query = "insert into cafes (url,latitude,longitude) values(?,?,?);"
+                const values = [url,parseFloat(latitude),parseFloat(longitude)];
+                const [result] = await connection.query(query,values);
+            }catch(queryError){
+                console.error(queryError);
+            }
         }
     }
     await browser.close();
     await connection.end();
     console.log("データが取得された");
+} catch(error) {
+    console.error("Error",error);
+}
 })();
