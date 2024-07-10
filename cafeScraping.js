643e6384f62aa1416a072f9759eb65212129fa01
noguchi-taiki <taiki.13n1015@gmail.com>
2024-07-11 05:25:37 +0900

    ローカルのmysqlへデータの保存性sこう。 今後：各ページでできるように修正する

--- cafeScraping.js
+++ cafeScraping.js
@@ -1,40 +1,33 @@
 const puppeteer = require('puppeteer');
 const mysql = require('mysql2/promise');
-const { headers } = require('next/headers');
-const { isAwaitExpression } = require('typescript');
+const { createConnection } = require('mysql2');
 
 (async()=> {
     const browser = await puppeteer.launch({ headless: false });
     try{
-    const connection = await mysql.createConnection({
-        host: "127.0.0.1",
-        // prot: "3306",ポート番号は以前は必要だったらしいけど今はいらない（書くとエラーになる）
-        database: "cafe_db",
-        user: "root",
-        password: "root",
-    })
     console.log("mysqlに接続");
     console.log("ページにアクセス");
 
     const page = await browser.newPage();
     await page.goto('https://www.clubjt.jp/place/spot/pref-13/');
     await Promise.all([
-        // page,waitForNavigation({waitUntil:`load`}),
+        page.waitForSelector(`a.tile-2col-link`),
         page.click("button.jtoc-adult-auth-modal__button-yes"),
         page.click(`a.tile-2col-link`),
     ])
      await Promise.all([
-        page.waitForNavigation({waitUntil:`load`}),
+        page.waitForSelector(`a.tile-2col-link`),
         page.click(`a.tile-2col-link`),
      ])
 
     //  const listSelector = "some selector";
     //puppeteerにおけるデータの取得は様々やり方があるので都度調べる必要がありそう。
-    //下記はxpathでデータを取得してそこからecaluateで文字を持ってきてる（備忘録）
+    //下記はxpathでデータを取得してそこからecaluateで文字を持ってきてる（いつぞやの備忘録）
 
     let i = 1;
-    const spotNames = [];
-    const tobaccoDetails = [];
+    const names = [];
+    const details = [];
+    const addresses = [];
 
     const itemsElems = await page.$$(".search-spot-item");
     while(i < itemsElems.length){
@@ -43,22 +36,44 @@ const { isAwaitExpression } = require('typescript');
         if(smokingIcon != null){
             const spotNameElem = await elem.$(".search-spot-link .search-spot-body .search-spot-name");
             const spotNameText = await spotNameElem.evaluate(el => el.textContent);
-            spotNames.push(spotNameText);
+            names.push(spotNameText);
 
             const tobaccoDetailElem1 = await elem.$(".search-spot-link .search-spot-tag .tobacco");
             let tobaccoDetailText = await tobaccoDetailElem1.evaluate(el => el.textContent);
             tobaccoDetailText += ",";
             const tobaccoDetailElem2 = await elem.$(".search-spot-link .search-spot-tag .tag");
             tobaccoDetailText += await tobaccoDetailElem2.evaluate(el => el.textContent);
-            tobaccoDetails.push(tobaccoDetailText);
+            details.push(tobaccoDetailText);
+
+            const addresElem = await elem.$(".search-spot-link .search-spot-body .search-spot-shopinfo");
+            let addressText = await addresElem.evaluate(el => el.textContent);
+            addressText = addressText.replace(/\n/g,"");
+            addressText = addressText.replace(/ /g,"");
+            addresses.push(addressText);
         }
         i = i+1;
     }
+    console.log("while文及びbreake文は正常に動作");
 
-    console.log(spotNames);
-    console.log(tobaccoDetails);
+    const connection = await mysql.createConnection({
+        host: "127.0.0.1",
+        // prot: "3306",非推奨らしいので書かない
+        database: "cafe_db",
+        user: "root",
+        password: "root",
+    })
+    i=0;
+    while(i < names.length){
+        let sql = `insert into tokyo values("${addresses[i]}","${names[i]}","${details[i]}");`
+        const reslt = connection.query(sql);
+        i++;
+    }
+    connection.end();
     
-    console.log("while文及びブレイク文は正常に動作");
+//今回のようにDBに1種類しかqueryを飛ばさないものならこの書き方でもいいけど
+//今後複数回DBのqueryを使うwebアプリの時はDB.js(DB.ts)のように、
+//コンポーネントとして分けてその場でsql文を引数として渡す方が良いっぽい
+
 } catch(error) {
     console.error("Error",error);
 } finally {
