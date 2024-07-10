1a870caa7939cedfbda528a4d7eada70e1ed4a1d
noguchi-taiki <taiki.13n1015@gmail.com>
2024-07-10 22:10:52 +0900

    クラス名にcafeの入っているスポットだけを取得し、カフェの名前とタバコの詳細(紙巻or電子 + 喫煙ブースor喫煙席)を取得する 今後：mysqlのDBのテーブルの設計とそれに伴ってcreate table文及びinsert文の追加。

--- cafeScraping.js
+++ cafeScraping.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 const mysql = require('mysql2/promise');
 const { headers } = require('next/headers');
+const { isAwaitExpression } = require('typescript');
 
 (async()=> {
     const browser = await puppeteer.launch({ headless: false });
@@ -13,6 +14,7 @@ const { headers } = require('next/headers');
         password: "root",
     })
     console.log("mysqlに接続");
+    console.log("ページにアクセス");
 
     const page = await browser.newPage();
     await page.goto('https://www.clubjt.jp/place/spot/pref-13/');
@@ -21,7 +23,6 @@ const { headers } = require('next/headers');
         page.click("button.jtoc-adult-auth-modal__button-yes"),
         page.click(`a.tile-2col-link`),
     ])
-    console.log("ページにアクセス");
      await Promise.all([
         page.waitForNavigation({waitUntil:`load`}),
         page.click(`a.tile-2col-link`),
@@ -32,23 +33,31 @@ const { headers } = require('next/headers');
     //下記はxpathでデータを取得してそこからecaluateで文字を持ってきてる（備忘録）
 
     let i = 1;
-    // const xpath = `/html/body/div[2]/main/div[5]/div[${i}]/a/div[1]/div[2]`;
-    // const elem = await page.waitForSelector(`::-p-xpath(${xpath})`);//$x()はバージョンの関係で使えない
-    // const text = await elem.evaluate(el => el.textContent);
-    // console.log(text);
+    const spotNames = [];
+    const tobaccoDetails = [];
 
-    while(true){
-        const xpath = `/html/body/div[2]/main/div[5]/div[${i}]/a/div[1]/div[2]`;
-        const iconXpsath = `/html/body/div[2]/main/div[5]/div[${i}]/a/div[1]/div[1]`;
-        const iconElem = await page.$(`::-p-xpath(${iconXpsath})`);
-        console.log(iconElem);
-        const elem = await page.$(`::-p-xpath(${xpath})`);
-        if(elem == null){break;}
-        const text = await elem.evaluate(el => el.textContent);
-        console.log(text);
-        console.log(i);
+    const itemsElems = await page.$$(".search-spot-item");
+    while(i < itemsElems.length){
+        const elem = itemsElems[i];
+        const smokingIcon = await elem.$(".search-spot-link .search-spot-body .cafe");
+        if(smokingIcon != null){
+            const spotNameElem = await elem.$(".search-spot-link .search-spot-body .search-spot-name");
+            const spotNameText = await spotNameElem.evaluate(el => el.textContent);
+            spotNames.push(spotNameText);
+
+            const tobaccoDetailElem1 = await elem.$(".search-spot-link .search-spot-tag .tobacco");
+            let tobaccoDetailText = await tobaccoDetailElem1.evaluate(el => el.textContent);
+            tobaccoDetailText += ",";
+            const tobaccoDetailElem2 = await elem.$(".search-spot-link .search-spot-tag .tag");
+            tobaccoDetailText += await tobaccoDetailElem2.evaluate(el => el.textContent);
+            tobaccoDetails.push(tobaccoDetailText);
+        }
         i = i+1;
     }
+
+    console.log(spotNames);
+    console.log(tobaccoDetails);
+    
     console.log("while文及びブレイク文は正常に動作");
 } catch(error) {
     console.error("Error",error);
