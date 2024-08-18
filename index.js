a6091917d96222837a825d4cfb283628776b9dc0
noguchi-taiki <taiki.13n1015@gmail.com>
2024-08-18 15:58:37 +0900

    ページ内に複数ページあるときに自動的に次ページへ遷移し ない場合もしくは最後のページの場合は処理を終わり元のページの次のページへ移動する。

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { createConnection } = require('mysql2');
 
 (async()=> {
     const browser = await puppeteer.launch({ 
-        headless: false,
+        headless: true,
         // args: [
         //     '--disable-features=IsolateOrigins,site-per-process',
         //     '--disable-web-security',
@@ -15,8 +15,8 @@ const { createConnection } = require('mysql2');
     
     
     try{
-    console.log("mysqlに接続");
-    console.log("ページにアクセス");
+    // console.log("mysqlに接続");
+    // console.log("ページにアクセス");
 
     const page = await browser.newPage();
 
@@ -32,7 +32,7 @@ const { createConnection } = require('mysql2');
     await page.goto('https://www.clubjt.jp/place/spot/pref-13/');
 
     let citys = await page.$$eval('a.tile-2col-link',list => {return list.map(data => data.href)});
-    console.log(citys[0]);
+    // console.log(citys[0]);
 
     await Promise.all([
         page.waitForNavigation({ waitUntil:'load'}),
@@ -61,36 +61,39 @@ const { createConnection } = require('mysql2');
 
 
 
+    let nextPage = await page.$('a.btn-small-normal');
     let k=0;
     while(k<links.length){ 
-        console.log(k);
-    if(k>=1){
+    if(k>=1&&nextPage==null){
         await Promise.all([
             page.goto(links[k]),
             page.waitForNavigation({ waitUntil:'load'}),
         ])}
-    let i = 1;
-    const names = [];
-    const details = [];
-    const addresses = [];
+        console.log(k+1 +' ページ目に移動しました。');
 
-    const itemsElems = await page.$$(".search-spot-item");
+    let i = 0;
+    let itemsElems = await page.$$(".search-spot-item");
+    let names = [];
+    let details = [];
+    let addresses = [];
+
     while(i < itemsElems.length){
-        const elem = itemsElems[i];
-        const smokingIcon = await elem.$(".search-spot-link .search-spot-body .cafe");
+        let elem = itemsElems[i];
+
+        let smokingIcon = await elem.$(".search-spot-link .search-spot-body .cafe");
         if(smokingIcon != null){
-            const spotNameElem = await elem.$(".search-spot-link .search-spot-body .search-spot-name");
-            const spotNameText = await spotNameElem.evaluate(el => el.textContent);
+            let spotNameElem = await elem.$(".search-spot-link .search-spot-body .search-spot-name");
+            let spotNameText = await spotNameElem.evaluate(el => el.textContent);
             names.push(spotNameText);
 
-            const tobaccoDetailElem1 = await elem.$(".search-spot-link .search-spot-tag .tobacco");
+            let tobaccoDetailElem1 = await elem.$(".search-spot-link .search-spot-tag .tobacco");
             let tobaccoDetailText = await tobaccoDetailElem1.evaluate(el => el.textContent);
             tobaccoDetailText += ",";
             const tobaccoDetailElem2 = await elem.$(".search-spot-link .search-spot-tag .tag");
             tobaccoDetailText += await tobaccoDetailElem2.evaluate(el => el.textContent);
             details.push(tobaccoDetailText);
 
-            const addresElem = await elem.$(".search-spot-link .search-spot-body .search-spot-shopinfo");
+            let addresElem = await elem.$(".search-spot-link .search-spot-body .search-spot-shopinfo");
             let addressText = await addresElem.evaluate(el => el.textContent);
             addressText = addressText.replace(/\n/g,"");
             addressText = addressText.replace(/ /g,"");
@@ -105,7 +108,31 @@ const { createConnection } = require('mysql2');
         console.log(names[i]);
         i++;
     }
-    k++;await page.goto(citys[0]);}
+
+    nextPage = await page.$('a.btn-small-normal');
+    if(nextPage!=null){
+        let nowPageELem = await page.$('span.btn-small-high-txt');
+        let nowpageNum = await nowPageELem.evaluate(el => el.textContent);
+        let nextPagesArray = await page.$$eval('a.btn-small-normal',(nextPagesArray => {return nextPagesArray.map(nextPagesArray=>nextPagesArray.href)}));
+        let nextPagesnum = await page.$$eval('a.btn-small-normal',(nextPagesArray => {return nextPagesArray.map(nextPagesArray=>nextPagesArray.textContent)}));
+        for(let i=0;i<nextPagesnum.length;i++){nextPagesnum[i] =  Number(nextPagesnum[i]);}
+        nowpageNum = Number(nowpageNum);
+        if(nextPagesnum.indexOf(nowpageNum+1) != -1){
+            tmp = nextPagesnum.indexOf(nowpageNum+1);
+            Promise.all([
+                await page.goto(nextPagesArray[tmp]),
+                page.waitForNavigation({ waitUntil:'load'}),
+                itemsElems = await page.$$(".search-spot-item"),
+            ])
+            names = [];
+            details = [];
+            addresses = [];
+        } else {console.log('not difind next page');nextPage=null;k++;await page.goto(citys[0]);}
+    } else {console.log('not difind next page');nextPage=null;k++;await page.goto(citys[0]);}
+}
+
+
+    // k++;await page.goto(citys[0]);}
 
     const connection = await mysql.createConnection({
         host: "127.0.0.1",
